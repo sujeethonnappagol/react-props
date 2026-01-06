6e3816319484308b027299579e837f2c4f0e3d66
sujeet <sujeethonnappagol3@gmail.com>
2026-01-06 23:55:01 +0530

    'react-components'

---
+++ src/App.jsx
@@ -0,0 +1,10 @@
+import Card from "./Components/Card"
+function App(){
+  return (
+    <>
+      <Card />
+    </>
+  )
+}
+
+export default App;
---
+++ src/Components/Card.jsx
@@ -0,0 +1,10 @@
+export default function Card(){
+    return (
+        <>
+          <div className="border-2 w-[300px] m-5 p-4 rounded-lg border-gray-400">
+             <img className='w-full h-auto rounded-md' src="https://images.unsplash.com/photo-1767519818605-b9f0c9e3d16a?w=600&auto=format&fit=crop&q=60&ixlib=rb-4.1.0&ixid=M3wxMjA3fDB8MHxmZWF0dXJlZC1waG90b3MtZmVlZHw1MXx8fGVufDB8fHx8fA%3D%3D" alt="" />
+             <p className="mt-2 text-md">Lorem ipsum dolor sit amet consectetur adipisicing elit.</p>
+          </div>
+        </>
+    )
+}
